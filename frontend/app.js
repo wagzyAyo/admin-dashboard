a11bacefdd5ec1e2cf74b88633ae4b5c7b8f5988
Ayo <talktojmcvibes@gmail.com>
2024-02-18 18:34:42 +0100

    append data

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,16 @@
 //get data from backend
 fetch('/getdata')
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+    data.forEach(item => {
+        const div = document.createElement("div");
+        div.textContent = `Name: ${item.name}
+        Amount:  ${item.amount}
+        Size: ${item.size}
+        Location: ${item.location}
+        short: ${item.short} 
+        Description: ${item.description}`;
+        document.body.appendChild(div)
+    })
+})
 .catch(error => console.log(error))
